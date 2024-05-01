f8f1fff51dd49d4bcc934ba413c6c2c11d542e90
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-02 00:01:29 +0530

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,96 @@
 import { useState } from 'react'
 import './App.css';
 import Checkout from './checkout';
+import Total from './Total_Price';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Checkout/>
+      <Checkout Products = {Products}/>
+      <Total/>
     
     </>
   )
 }
 
 export default App;
+
+
+
+const Products = [
+  {
+      "id": 1,
+      "title": "iPhone 15",
+      "description": "An apple mobile which is nothing like apple",
+      "starrating" :"⭐⭐⭐⭐⭐" ,
+      "price": 799,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+      "images": "https://m.media-amazon.com/images/I/71657TiFeHL._AC_UY327_FMwebp_QL65_.jpg",
+
+   },
+   {
+    "id": 2,
+    "title": "iPhone X",
+    "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+    "starrating" :"⭐⭐⭐⭐" ,
+    "price": 899,
+    "discountPercentage": 17.94,
+    "rating": 4.44,
+    "stock": 34,
+    "brand": "Apple",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+    "images": "https://rukminim2.flixcart.com/image/312/312/j9d3bm80/mobile/f/y/v/apple-iphone-x-mqa92hn-a-original-imaeyysgqbg8qmhn.jpeg?q=70",
+},
+{
+    "id": 3,
+    "title": "Samsung Universe 9",
+    "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+    "starrating" :"⭐⭐⭐⭐" ,
+    "price": 1249,
+    "discountPercentage": 15.46,
+    "rating": 4.09,
+    "stock": 36,
+    "brand": "Samsung",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+    "images": "https://rukminim2.flixcart.com/image/312/312/jeiukcw0/mobile/q/g/h/samsung-galaxy-s9-plus-sm-g965fzbdins-original-imaf372gzm4hg4eh.jpeg?q=70"
+    
+},
+{
+    "id": 4,
+    "title": "OPPOF19",
+    "description": "OPPO F19 is officially announced on April 2021.",
+    "starrating" :"⭐⭐⭐⭐⭐" ,
+    "price": 280,
+    "discountPercentage": 17.91,
+    "rating": 4.3,
+    "stock": 123,
+    "brand": "OPPO",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+    "images": "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/5/x/0/-original-imaggvy34uxyxnzz.jpeg?q=70"
+},
+{
+    "id": 5,
+    "title": "Huawei P30",
+    "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+    "starrating" :"⭐⭐⭐" ,
+    "price": 499,
+    "discountPercentage": 10.58,
+    "rating": 4.09,
+    "stock": 32,
+    "brand": "Huawei",
+    "category": "smartphones",
+    "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+    "images": "https://rukminim2.flixcart.com/image/612/612/k2jbyq80pkrrdj/mobile-refurbished/x/y/b/p30-lite-128-c-marie-l22a-huawei-4-original-imafghwdpthze4pb.jpeg?q=70"
+
+}
+]
---
+++ src/Total_Price.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Total_Price = () => {
+  return (
+    <div className="grand_container">
+
+<div className="Parent_container">
+       <div className="section_middle">
+    <div className="price_calculation">
+    <div>subtotal : </div>
+    <h3>$ 200</h3>
+    </div>
+    <div className="price_calculation">
+    <div>Shipping : </div>
+    <h3>$ 10</h3>
+    </div>
+  </div>
+
+
+  <div className="section_bottom">
+    <div>Total : </div>
+    <h3>234 $</h3>
+  </div>
+
+
+</div>
+      
+    // </div>
+  )
+}
+
+export default Total_Price
+
