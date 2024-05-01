2d2696279c9a7d4bcac3df8c7d3cfc990fb444e1
Selvakumar Boominathan <selvans2k@gmail.com>
2024-05-01 20:54:30 +0530

    style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import Checkout from './checkout';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello</h1>
+      <Checkout/>
     
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/checkout.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import './checkout.css';
+
+
+export default function Checkout() {
+
+  return (
+
+<div className="grand_container">
+<div className="Parent_container">
+
+  <div className="Section_top"> 
+
+    <div className="Img_container">
+    <img src="https://rukminim2.flixcart.com/image/612/612/l2krs7k0/regionalbooks/2/v/n/fundamentals-of-geography-by-study-iq-original-imagdw8ygmyzstk6.jpeg?q=70"></img>
+    </div>
+    
+    <div className="productDetails_container">
+      <h2>WOLF so Grim</h2>
+      <h6>Details and care</h6>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse libero, consectetur, facere voluptatem vero eum necessitatibus.</p>
+      <h6>sustainability</h6>
+    </div>
+
+    <div className="price_details">
+      <div>
+        <span>1</span>
+        <span>price</span>
+      </div>
+      <div>
+        Remove
+      </div>
+    </div>
+
+  </div>
+
+
+  <div className="section_middle">
+    <div className="price_calculation">
+    <div>subtotal : </div>
+    <div>200$</div>
+    </div>
+    <div className="price_calculation">
+    <div>Shipping : </div>
+    <div>10$</div>
+    </div>
+  </div>
+
+
+  <div className="section_bottom">
+    <div>Total : </div>
+    <div>234 $</div>
+  </div>
+
+
+</div>
+</div>
+)}
+
+
+
+
+
+
+
+const Product = 
+  {
+      "id": 1,
+      "title": "iPhone 15",
+      "description": "An apple mobile which is nothing like apple",
+      "price": 71999,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+      "images": "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/j/z/3/-original-imagtc5fqyz8tu4c.jpeg?q=70",
+
+   }
+//   {
+//       "id": 2,
+//       "title": "iPhone X",
+//       "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+//       "price": 899,
+//       "discountPercentage": 17.94,
+//       "rating": 4.44,
+//       "stock": 34,
+//       "brand": "Apple",
+//       "category": "smartphones",
+//       "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+//       "images": "https://rukminim2.flixcart.com/image/312/312/j9d3bm80/mobile/f/y/v/apple-iphone-x-mqa92hn-a-original-imaeyysgqbg8qmhn.jpeg?q=70",
+//   },
+//   {
+//       "id": 3,
+//       "title": "Samsung Universe 9",
+//       "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+//       "price": 1249,
+//       "discountPercentage": 15.46,
+//       "rating": 4.09,
+//       "stock": 36,
+//       "brand": "Samsung",
+//       "category": "smartphones",
+//       "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+//       "images": "https://rukminim2.flixcart.com/image/312/312/jeiukcw0/mobile/q/g/h/samsung-galaxy-s9-plus-sm-g965fzbdins-original-imaf372gzm4hg4eh.jpeg?q=70"
+      
+//   },
+//   {
+//       "id": 4,
+//       "title": "OPPOF19",
+//       "description": "OPPO F19 is officially announced on April 2021.",
+//       "price": 280,
+//       "discountPercentage": 17.91,
+//       "rating": 4.3,
+//       "stock": 123,
+//       "brand": "OPPO",
+//       "category": "smartphones",
+//       "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+//       "images": "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/5/x/0/-original-imaggvy34uxyxnzz.jpeg?q=70"
+//   },
+//   {
+//       "id": 5,
+//       "title": "Huawei P30",
+//       "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+//       "price": 499,
+//       "discountPercentage": 10.58,
+//       "rating": 4.09,
+//       "stock": 32,
+//       "brand": "Huawei",
+//       "category": "smartphones",
+//       "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+//       "images": "https://rukminim2.flixcart.com/image/612/612/k2jbyq80pkrrdj/mobile-refurbished/x/y/b/p30-lite-128-c-marie-l22a-huawei-4-original-imafghwdpthze4pb.jpeg?q=70"
+
+//   }
+// ]
